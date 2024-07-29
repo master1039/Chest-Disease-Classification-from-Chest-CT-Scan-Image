1df203c6ffd2a07fc66a13daf45ae8da6c3abddf
master1039 <saurabh.sinha212@gmail.com>
2024-07-29 15:27:35 +0530

    folder structure updated

---
+++ template.py
@@ -0,0 +1,35 @@
+import os
+from pathlib import Path
+
+project_name = 'cnnClassifier'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py"
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/constants/__init__.py.py",
+    "requirements.txt",
+    "setup.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+    else:
+        print(f"File {filename} already exists at {filepath}")
